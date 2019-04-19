1334b3b8b9a26f45d831de8f92c012db9be7a786
Gustavo Spredemann <g.spredemann@gmail.com>
2019-04-19 13:15:58 -0300

    make all functions pure

--- src/algorithms/shortestPath.ts
+++ src/algorithms/shortestPath.ts
@@ -50,6 +50,67 @@ function extractMin(queue: any, d: any): NodeKey | null {
   return minNode
 }
 
+function dijkstra(source: NodeKey, graph: Graph) {
+  // Predecessors.
+  const predecessors: any = {}
+
+  // Upper bounds for shortest path weights from source.
+  const d: any = initializeSingleSource(source, graph)
+  // Poor man's priority queue, keyed on d.
+  let queue: any = initializePriorityQueue(graph)
+
+  function relax(u: NodeKey, v: NodeKey) {
+    // var w = getEdgeWeight(u, v)
+    const w = 1
+    if (d[v] > d[u] + w) {
+      d[v] = d[u] + w
+      predecessors[v] = u
+    }
+  }
+
+  while (!priorityQueueEmpty(queue)) {
+    const u = extractMin(queue, d)
+
+    if (u === null) {
+      queue = {}
+      break
+    }
+
+    const node = graph.getNode(u)
+
+    node &&
+      node.children.forEach((v) => {
+        relax(node.key, v.key)
+      })
+  }
+
+  return predecessors
+}
+
+// Assembles the shortest path by traversing the
+// predecessor subgraph from destination to source.
+function path(source: NodeKey, destination: NodeKey, predecessors: any) {
+  const nodeList = []
+  let weight = 0
+  let node = destination
+  while (predecessors[node]) {
+    nodeList.push(node)
+    // weight += getEdgeWeight(p[node], node)
+    weight += 1
+    node = predecessors[node]
+  }
+  if (node !== source) {
+    throw new Error('No path found')
+  }
+  nodeList.push(node)
+  nodeList.reverse()
+
+  return {
+    weight,
+    path: nodeList,
+  }
+}
+
 export function shortestPath(source: NodeKey, destination: NodeKey, graph: Graph) {
   const error = !graph.getNode(source)
     ? 'Source node is not in the graph.'
@@ -61,67 +122,5 @@ export function shortestPath(source: NodeKey, destination: NodeKey, graph: Graph
     throw new Error(error)
   }
 
-  // Predecessors.
-  const predecessors: any = {}
-
-  function dijkstra() {
-    // Upper bounds for shortest path weights from source.
-    const d: any = initializeSingleSource(source, graph)
-    // Poor man's priority queue, keyed on d.
-    let queue: any = initializePriorityQueue(graph)
-
-    function relax(u: NodeKey, v: NodeKey) {
-      // var w = getEdgeWeight(u, v)
-      const w = 1
-      if (d[v] > d[u] + w) {
-        d[v] = d[u] + w
-        predecessors[v] = u
-      }
-    }
-
-    while (!priorityQueueEmpty(queue)) {
-      const u = extractMin(queue, d)
-
-      if (u === null) {
-        queue = {}
-        break
-      }
-
-      const node = graph.getNode(u)
-
-      node &&
-        node.children.forEach((v) => {
-          relax(node.key, v.key)
-        })
-    }
-  }
-
-  // TODO: move this out of this function
-  // Assembles the shortest path by traversing the
-  // predecessor subgraph from destination to source.
-  function path() {
-    const nodeList = []
-    let weight = 0
-    let node = destination
-    while (predecessors[node]) {
-      nodeList.push(node)
-      // weight += getEdgeWeight(p[node], node)
-      weight += 1
-      node = predecessors[node]
-    }
-    if (node !== source) {
-      throw new Error('No path found')
-    }
-    nodeList.push(node)
-    nodeList.reverse()
-
-    return {
-      weight,
-      path: nodeList,
-    }
-  }
-
-  dijkstra()
-
-  return path()
+  return path(source, destination, dijkstra(source, graph))
 }
